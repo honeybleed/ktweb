8c7b2ba5ff10142580cbc5107e62777f74f4e43f
Aaron <honeybleed@gmail.com>
2018-06-15 17:55:26 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import './style.scss';
+
+let action = (log)=>{
+    console.log("LOG:" ,log);
+};
+action("hello world!");
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: "development",
+    devtool: "source-map",
+    target: "web",
+    watch: true,
+    entry: {
+        index: path.join(__dirname, 'src/index.ts')
+    },
+    optimization: {
+
+    },
+    output: {
+        filename: "[name].js",
+        publicPath: "/public/",
+        path: path.join(__dirname, 'public/')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss?$/,
+                use:[
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            sourceMap: true,
+                            config: {
+                                path: './postcss.config.js'
+                            }
+                        }
+                    },
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    devServer: {
+        port: 8080,
+        inline: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename:"style.css"
+        })
+    ]
+};
